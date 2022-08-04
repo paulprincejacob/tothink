314ad055a7765f159ddff88e9f2da113c0e6ed74
paul.p <paul.p@tataelxsi.co.in>
2022-08-04 16:49:28 +0530

    Landing Page added

---
+++ lib/features/presentation/pages/landingPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Landingpage extends StatefulWidget {
+  const Landingpage({Key? key}) : super(key: key);
+
+  @override
+  State<Landingpage> createState() => _LandingpageState();
+}
+
+class _LandingpageState extends State<Landingpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tothink/features/presentation/pages/landingPage.dart';
 import 'package:tothink/mainPage/mainScreen.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.green,
       ),
-      home: const Mainscreen(),
+      home: const Landingpage(),
     );
   }
 }
